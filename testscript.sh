e29b8b51fc0c4e15d01de5288774bb2283de1d22
Emanuel Wieland <60539146+EmanuelWieland@users.noreply.github.com>
2023-02-19 16:00:01 +0100

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "this script is from github"
-NUMBER=$((NUMBERONE + $NUMBERTWO))
+NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "this test22 is complete"
 echo "this was run automatically"
