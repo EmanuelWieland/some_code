a8fb3ea03dd5f058df1a781f2fe05fa08966287e
Emanuel Wieland <emanuel.wieland@gmail.com>
2023-02-19 12:05:05 +0100

    Adding inital files

--- testscript.sh
+++ testscript.sh
@@ -2,5 +2,5 @@
 echo "this script is from github"
 NUMBER=$((NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-echo "this test is complete"
+echo "this test22 is complete"
 echo "this was run automatically"
